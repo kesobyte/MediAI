9dc601f7ebdffcdb086ee1c921e1cc81815fb199
Kim Angelo Alvarez <kimangeloalvarez@gmail.com>
2024-12-04 14:36:57 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -186,10 +186,10 @@ app.use((err, req, res, next) => {
 });
 
 // PRODUCTION
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
 
 app.listen(port, () => {
